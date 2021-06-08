85da71e57acfeb5ceaf4ba90bf7880fc741051b4
SBilos <85586536+SBilos@users.noreply.github.com>
2021-06-08 23:50:45 +0300

    Add files via upload

---
+++ Task12.java
@@ -0,0 +1,11 @@
+package work12;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println(" + + +      +      +       +       +");
+        System.out.println("     +     + +      +     +       + +");
+        System.out.println("     +    +   +      +   +       +   +");
+        System.out.println("+    +   + + + +      + +       + + + +");
+        System.out.println(" + +    +       +      +       +       +");
+    }
+}
