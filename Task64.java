7d61bac49460f72a7e54a196740e508d9bd7a7fd
SBilos <85586536+SBilos@users.noreply.github.com>
2021-06-23 21:54:03 +0300

    Add files via upload

---
+++ Task64.java
@@ -0,0 +1,39 @@
+package work64;
+
+import java.util.Scanner;
+
+public class Task64 {
+    public static void main(String[] args) {
+    int number = getNumber();
+    String binaryNumber = convertDecimalToBinary(number);
+    System.out.println(binaryNumber);
+    }
+
+    public static String convertDecimalToBinary(int number) {
+        String result = "";
+        do {
+            int digit = number % 2;
+            result = digit + result;
+            number /= 2;
+        } while (number > 0);
+        return result;
+    }
+
+    public static int getNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number -> ");
+        while (true) {
+            if (!scanner.hasNextInt()) {
+                scanner.next();
+            } else {
+                int number = scanner.nextInt();
+                if (number > 0) {
+                    return number;
+                }
+            }
+            System.out.println("Invalid value! Repeat!");
+        }
+
+    }
+
+}
